baf2c9587a4c7f43e2163fec8f09c4b6dcf41546
Paula Gonzalez <gonzaleztoscano.pau@gmail.com>
2024-11-27 15:22:31 -0300

    OK

--- src/components/MenuPrincipal.tsx
+++
@@ -1,7 +0,0 @@
-import React from 'react';
-
-const MenuPrincipal: React.FC = () => {
-  return <div>Bienvenido al Menú Principal</div>;
-};
-
-export default MenuPrincipal;
--- src/types.ts
+++
@@ -1,9 +0,0 @@
-export interface Producto {
-  id: number;
-  nombre: string;
-  imagen: string;
-  precio: number;
-  cantidad: number;
-}
-
-export type ResumenState = Record<number, Producto>;
